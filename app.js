a28301c2c0dc9b07ccce0f4be9d992cb18c73df3
jidekickpush <jeandenisdrane@gmail.com>
2022-04-20 12:50:56 +0200

    implement hbs built-in for the products

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
 const express = require('express');
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
 app.use(express.static('public'));
 
 // app.get(path,code);
 
-app.get("/about", (req, res)=>{
+app.get("/",(req, res, next)=>{
+    res.sendFile(__dirname + "/views/home.html");
+})
+
+app.get("/about", (req, res, next)=>{
     // console.log(req.url);
     // console.log(req.method);
     res.sendFile(__dirname + "/views/about.html");
-    console.log("a request on the ABOUT page was received...");
 });
 
 
 app.get("/contact", (req, res, next)=> {
-    res.sendFile(__dirname + "/views/contact.html");
     console.log("a request on the CONTACT page was received...");
+    res.sendFile(__dirname + "/views/contact.html");
+});
 
+app.get("/limoncello", (req, res, next) => {
+
+    const data = {
+        title: "Limoncello",
+        price: 20,
+        imageFile: "limoncello1.jpeg",
+        stores: ["Online","Paris", "Albacete","London"],
+    }
+
+    res.render("product", data);
+});
+
+app.get("/single-malt", (req, res, next)=> {
+    const data = {
+        title: "Single Malt",
+        price: 75,
+        imageFile:"single-malt1.jpeg",
+        stores: [],
+    }
+
+    res.render("product",data)
+});
+
+app.get("/tequila", (req, res, next)=> {
+    const data = {
+        title: "Tequila",
+        price: 25,
+        imageFile:"tequila1.jpeg",
+        stores: ["Online","London"]
+    }
+
+    res.render("product",data)
 });
 
 
